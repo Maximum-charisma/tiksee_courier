c10f9f7237f4c78e108371db52d1532c68d1f7d1
Maxim <maximum-charisma@gmail.com>
2022-02-03 14:47:49 +0300

    account screen design

--- lib/screens/bottom_nav/account_screen.dart
+++ lib/screens/bottom_nav/account_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tiksee_courier/services/account_card.dart';
+import 'package:tiksee_courier/services/app_bar.dart';
+import 'package:tiksee_courier/services/settings_card.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({Key? key}) : super(key: key);
@@ -10,6 +13,26 @@ class AccountScreen extends StatefulWidget {
 class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          StandartAppBar(
+            title: const Text('Аккаунт'),
+          ),
+          const SliverToBoxAdapter(
+            child: AccountCard(),
+          ),
+          SliverToBoxAdapter(
+            child: SettingsCard(
+              icon: Icons.logout,
+              text: 'Выйти',
+              onTap: () {
+                // provider.signOutCourier();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
